452e2277ec7134b814d2e11d572e175a39a919a5
Yann Dirson <ydirson@free.fr>
2021-07-31 07:11:37 -0400

    fastlogs: send logs to stderr not stdout, on non-android platforms

--- common/fastlog.h
+++ common/fastlog.h
@@ -2,6 +2,8 @@
 #include <android/log.h>
 
 #define TAG "GLStreamingNative"
+#else
+#include <stdio.h>
 #endif
 
 #ifdef __cplusplus
@@ -14,10 +16,10 @@ extern "C" {
   #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,     TAG, __VA_ARGS__)
   #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,    TAG, __VA_ARGS__)
 #else
-  #define LOGE(...) printf(__VA_ARGS__)
-  #define LOGW(...) printf(__VA_ARGS__)
-  #define LOGI(...) printf(__VA_ARGS__)
-  #define LOGD(...) printf(__VA_ARGS__)
+  #define LOGE(...) fprintf(stderr, __VA_ARGS__)
+  #define LOGW(...) fprintf(stderr, __VA_ARGS__)
+  #define LOGI(...) fprintf(stderr, __VA_ARGS__)
+  #define LOGD(...) fprintf(stderr, __VA_ARGS__)
 #endif
 
 #ifdef __cplusplus
