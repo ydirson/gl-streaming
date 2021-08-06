96e24e25227668398ca0030c17fa6b4d37e841ba
Yann Dirson <ydirson@free.fr>
2021-08-06 17:30:01 -0400

    serveregl: fix ?: usage wrt casting args and return value

--- gl_server/servergles.c
+++ gl_server/servergles.c
@@ -681,7 +681,8 @@ void glse_glVertexAttribPointer()
     // int ptr_str_len = strnlen(c->ptr, 0xA00000);
     // LOGD("PTR Str len = %i\n", ptr_str_len);
     
-    glVertexAttribPointer(c->indx, c->size, c->type, c->normalized, c->stride, ((const GLvoid *) c->ptr_isnull == TRUE ? c->ptr_uint : c->ptr));
+    glVertexAttribPointer(c->indx, c->size, c->type, c->normalized, c->stride,
+                          ((const GLvoid *) (c->ptr_isnull == TRUE ? (void*)c->ptr_uint : c->ptr)));
 }
 
 
