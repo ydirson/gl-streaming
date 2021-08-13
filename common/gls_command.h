ydirson/gl-streaming

commit 2f33d84c9d258d0c7843e45aa32a67c8ad5b1143

[Client] Draw call changes... [All] Prevent crash when passing NULL pixel data to glTex(Sub)Image2D

common/gls_command.h
@@ -1111,6 +1111,7 @@ typedef struct
   uint32_t format;
   uint32_t type;
   char pixels[4];
+  uint32_t pixels_isnull;
 } gls_glTexImage2D_t;
 
 
@@ -1137,6 +1138,7 @@ typedef struct
   uint32_t format;
   uint32_t type;
   char pixels[4];
+  uint32_t pixels_isnull;
 } gls_glTexSubImage2D_t;
 
 

gl_client/Makefile
@@ -7,7 +7,7 @@ CFLAGS+=-DUSE_X11
 # CFLAGS+=-DGLS_USE_SRVSIZE
 
 # Try to emulate client arrays with VBO.
-# CFLAGS+=-DGLS_EMULATE_VBO
+CFLAGS+=-DGLS_EMULATE_VBO
 
 LIBS=-pthread -ldl -lX11
 CFLAGS+= $(LIBS)

gl_client/clientgles.c
@@ -436,15 +436,31 @@ GLvoid glDrawRangeElements( GLenum mode, GLuint start, GLuint end, GLsizei count
 
 
     GLS_SET_COMMAND_PTR_BATCH(c, glDrawElements);
-    c->mode = mode;
-    c->count = count;
-    c->type = type;
-  #if __WORDSIZE == 64
-    c->indices = (uint32_t)(uint64_t)indices;
-  #else
-    c->indices = (uint32_t)indices;
-  #endif
-    GLS_PUSH_BATCH(glDrawElements);
+	c->mode = mode;
+	c->count = count;
+	c->type = type;
+
+#ifdef GLS_EMULATE_VBO
+	c->indices_isnull = 1;
+	c->indices[0] = '\0';
+#else // !GLS_EMULATE_VBO
+	c->indices_isnull = (indices == NULL);
+	if (c->indices_isnull == FALSE) {
+		const GLchar *indices_str = (const GLchar *)indices;
+		memcpy(c->indices, indices_str, GLS_STRING_SIZE);
+	} else {
+		c->indices[0] = '\0';
+	}
+#endif // !GLS_EMULATE_VBO
+
+#if __WORDSIZE == 64
+	c->indices_uint = (uint32_t)(uint64_t)indices;
+#else // __WORDSIZE == 32
+	c->indices_uint = (uint32_t)indices;
+#endif // __WORDSIZE == 32
+	
+	GLS_PUSH_BATCH(glDrawElements);
+	
     if( !ibo_bkp )
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
        glBindBuffer( GL_ARRAY_BUFFER, vbo_bkp );
@@ -968,7 +984,10 @@ GL_APICALL void GL_APIENTRY glTexImage2D (GLenum target, GLint level, GLint inte
   c->border = border;
   c->format = format;
   c->type = type;
-  memcpy(c->pixels, pixels, datasize);
+  c->pixels_isnull = (pixels == NULL);
+  if (c->pixels_isnull == FALSE) {
+	  memcpy(c->pixels, pixels, datasize);
+  }
   push_batch_command(cmd_size);
   gls_cmd_flush();
 }
@@ -1033,7 +1052,10 @@ GL_APICALL void GL_APIENTRY glTexSubImage2D (GLenum target, GLint level, GLint x
   c->height = height;
   c->format = format;
   c->type = type;
-  memcpy(c->pixels, pixels, datasize);
+  c->pixels_isnull = (pixels == NULL);
+  if (c->pixels_isnull == FALSE) {
+	  memcpy(c->pixels, pixels, datasize);
+  }
   push_batch_command(cmd_size);
   gls_cmd_flush();
 }
@@ -1194,7 +1216,7 @@ GL_APICALL void GL_APIENTRY glVertexAttribPointer (GLuint indx, GLint size, GLen
 	c->ptr[0] = '\0';
 #else // !GLS_EMULATE_VBO
 	// 0xA0 glxgears crash?
-	c->ptr_isnull = (ptr == NULL || ptr == 0xc || ptr == 0xA20);
+	c->ptr_isnull = (ptr == NULL || ptr == 0xc || ptr == 0x2A0 || ptr == 0xA20);
 	// printf("gls glVertexAttribPointer: type=%p, ptr=%p, unptr_null=%p\n", type, ptr, *&ptr);
 	if (c->ptr_isnull == FALSE) {
 		const GLchar *ptr_str = (const GLchar *)ptr;
@@ -1212,12 +1234,16 @@ GL_APICALL void GL_APIENTRY glVertexAttribPointer (GLuint indx, GLint size, GLen
 	}
 #endif // !GLS_EMULATE_VBO
 
+/*
+	c->ptr_isnull = 1;
+	c->ptr[0] = '\0';
+
 #if __WORDSIZE == 64
 	c->ptr_uint = (uint32_t)(uint64_t)ptr;
 #else // __WORDSIZE == 32
 	c->ptr_uint = (uint32_t)ptr;
 #endif // __WORDSIZE == 32
-
+*/
 	c->normalized = normalized;
 	GLS_PUSH_BATCH(glVertexAttribPointer);
 }

gl_server/servergles.c
@@ -546,7 +546,7 @@ void glse_glTexParameteri()
 void glse_glTexImage2D()
 {
   GLSE_SET_COMMAND_PTR(c, glTexImage2D);
-  glTexImage2D(c->target, c->level, c->internalformat, c->width, c->height, c->border, c->format, c->type, c->pixels);
+  glTexImage2D(c->target, c->level, c->internalformat, c->width, c->height, c->border, c->format, c->type, c->pixels_isnull ? NULL : c->pixels);
 }
 
 
@@ -554,7 +554,7 @@ void glse_glTexImage2D()
 void glse_glTexSubImage2D()
 {
   GLSE_SET_COMMAND_PTR(c, glTexSubImage2D);
-  glTexSubImage2D(c->target, c->level, c->xoffset, c->yoffset, c->width, c->height, c->format, c->type, c->pixels);
+  glTexSubImage2D(c->target, c->level, c->xoffset, c->yoffset, c->width, c->height, c->format, c->type, c->pixels_isnull ? NULL : c->pixels);
 }
 
 

commit 020eb6150c0a83dd219efe65572d0e87b739542c

protocol: fix texture uploading
2f33d84c9d258d0c7843e45aa32a67c8ad5b1143 broke texture support by adding a
new data field inside of texture data.

common/gls_command.h
@@ -1266,8 +1266,8 @@ typedef struct
   int32_t border;
   uint32_t format;
   uint32_t type;
-  char pixels[4];
   uint32_t pixels_isnull;
+  char pixels[4];
 } gls_glTexImage2D_t;
 
 
@@ -1292,8 +1292,8 @@ typedef struct
   int32_t height;
   uint32_t format;
   uint32_t type;
-  char pixels[4];
   uint32_t pixels_isnull;
+  char pixels[4];
 } gls_glTexSubImage2D_t;
 
 