f6358797e1e06c4f52c227a9037204f5e46dc2ce
Yann Dirson <ydirson@free.fr>
2022-02-07 00:06:10 +0100

    server: introduce a polling-based event loop
    Allows to get rid of active-polling sleep, does improve throughput
    (with swrast backend, seen +18% in es2gears, +5% in glmark2).

--- gl_server/glserver.c
+++ gl_server/glserver.c
@@ -32,6 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "fastlog.h"
 
 #include <errno.h>
+#include <poll.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
@@ -95,8 +96,8 @@ void glse_cmd_CREATE_WINDOW(gls_command_t* buf)
 static void glse_handle_fifo_packet(recvr_context_t* rc)
 {
   void* popptr = (void*)fifo_pop_ptr_get(&rc->fifo);
-  if (popptr == NULL) {
-    usleep(SLEEP_USEC);
+  if (popptr == NULL) { // should not happen, poll() rocks
+    LOGW("GLS WARNING: glse_handle_fifo_packet called with empty fifo\n");
     return;
   }
 
@@ -147,9 +148,38 @@ void glserver_handle_packets(recvr_context_t* rc)
   glsec_global.pool.out_buf.buf = (char*)malloc(GLSE_OUT_BUFFER_SIZE);
   glsec_global.pool.out_buf.size = GLSE_OUT_BUFFER_SIZE;
 
+  enum {
+    POLLFD_FIFO,
+  };
+  struct pollfd pollfds[] = {
+    [POLLFD_FIFO] = {
+      .fd = rc->fifo.pipe_rd,
+      .events = POLLIN
+    },
+  };
 
   while (1) {
-    glse_handle_fifo_packet(rc);
+    int ret = poll(pollfds, sizeof(pollfds) / sizeof(pollfds[0]), -1);
+    if (ret < 0) {
+      LOGE("GLS ERROR: poll failed: %s\n", strerror(errno));
+      break;
+    }
+    assert(!(pollfds[POLLFD_FIFO].revents & POLLNVAL));
+
+    if (pollfds[POLLFD_FIFO].revents & POLLERR) {
+      LOGE("GLS ERROR: FIFO poll error\n");
+      break;
+    }
+    if (pollfds[POLLFD_FIFO].revents & POLLHUP) {
+      //LOGD("GLS DEBUG: FIFO poll hangup\n");
+      break;
+    }
+    if (pollfds[POLLFD_FIFO].revents & POLLIN) {
+      glse_handle_fifo_packet(rc);
+      pollfds[POLLFD_FIFO].revents &= ~POLLIN;
+    }
+    if (pollfds[POLLFD_FIFO].revents)
+      LOGW("GLS WARNING: FIFO poll revents=0x%x\n", pollfds[POLLFD_FIFO].revents);
   }
 
   release_egl(&gc);
