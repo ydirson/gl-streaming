9e327444a80968c204e29f90764c50cba6954071
Yann Dirson <ydirson@free.fr>
2022-03-23 21:28:49 +0100

    recvr: move server code to server/main.c
    Will make it easier to deal with server-side options without polluting
    common code.

--- common/recvr.h
+++ common/recvr.h
@@ -46,11 +46,9 @@ typedef struct
   struct gls_connection* cnx;
 } recvr_context_t;
 
-void recvr_server_start(recvr_context_t* rc, const char* server_addr,
-                        void(*handle_child)(recvr_context_t*));
-void recvr_connection_start(recvr_context_t* rc,
-                            void(*handle_child)(recvr_context_t*));
-void recvr_client_start(recvr_context_t* rc, const char* server_addr);
 void recvr_setup(recvr_context_t* rc);
 int recvr_handle_packet(recvr_context_t* rc);
+void* recvr_socket_to_fifo_loop(void* data);
 void recvr_stop(recvr_context_t* c);
+
+void recvr_client_start(recvr_context_t* rc, const char* server_addr);
--- gl_server/main.c
+++ gl_server/main.c
@@ -31,11 +31,67 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "glserver.h"
 
 #include <errno.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+// server settings
+#define FIFO_SIZE_ORDER 12
+#define FIFO_PACKET_SIZE_ORDER 15
+
+static void child_process(recvr_context_t* rc,
+                          void(*handle_child)(recvr_context_t*))
+{
+  fifo_init(&rc->fifo, FIFO_SIZE_ORDER, FIFO_PACKET_SIZE_ORDER);
+  pthread_create(&rc->recvr_th, NULL, recvr_socket_to_fifo_loop, rc);
+  pthread_setname_np(rc->recvr_th, "gls-recvr");
+  handle_child(rc);
+  if (pthread_join(rc->recvr_th, NULL) != 0)
+    LOGE("pthread_join failed\n");
+  recvr_stop(rc);
+  LOGI("client terminated\n");
+}
+
+static void recvr_server_start(recvr_context_t* rc, const char* server_addr,
+                               void(*handle_child)(recvr_context_t*))
+{
+  struct gls_server* srv = tport_server_create(server_addr);
+  if (!srv)
+    exit(EXIT_FAILURE);
+
+  int quit = 0;
+  while (!quit) {
+    rc->cnx = tport_server_wait_connection(srv);
+    if (!rc->cnx)
+      break;
+    LOGI("new client\n");
+
+    switch (fork()) {
+    case -1:
+      LOGE("%s: fork failed: %s\n", __FUNCTION__, strerror(errno));
+      break;
+    case 0:
+      free(srv);
+      child_process(rc, handle_child);
+      exit(EXIT_SUCCESS);
+    default:
+      tport_close(rc->cnx);
+      // ... loop and wait for a new client
+    }
+  }
+  free(srv);
+}
+
+// FIXME: could use server_addr -- worth it?
+static void recvr_connection_start(recvr_context_t* rc,
+                                   void(*handle_child)(recvr_context_t*))
+{
+  rc->cnx = tport_connection_create();
+  child_process(rc, handle_child);
+}
 
 int main(int argc, char* argv[])
 {
