8d8688fac836fca3e4904cc7ac5d3c8421a25dcc
Yann Dirson <ydirson@free.fr>
2022-04-17 16:32:44 +0200

    server: properly exit when ring_init() fails
    Even if today the failure path is not active, it's cleaner to have the exit
    at this level.

--- gl_server/main.c
+++ gl_server/main.c
@@ -48,7 +48,8 @@ static int nofork;
 static void child_process(recvr_context_t* rc,
                           void(*handle_child)(recvr_context_t*))
 {
-  ring_init(&rc->ring, RING_SIZE_ORDER, RING_PACKET_SIZE_ORDER);
+  if (ring_init(&rc->ring, RING_SIZE_ORDER, RING_PACKET_SIZE_ORDER) < 0)
+    exit(EXIT_FAILURE);
   recvr_run_loop(rc);
   handle_child(rc);
   if (pthread_join(rc->recvr_th, NULL) != 0)
